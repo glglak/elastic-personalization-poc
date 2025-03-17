52ac9c71bb1fb361ca297df8f109369f8b98fe9e
glglak <karem.kamel@gmail.com>
2025-03-17 21:10:31 +0200

    Add API Program.cs

---
+++ src/ElasticPersonalization.API/Program.cs
@@ -0,0 +1,43 @@
+using ElasticPersonalization.API.Extensions;
+using ElasticPersonalization.Core.Interfaces;
+using ElasticPersonalization.Infrastructure.Data;
+using ElasticPersonalization.Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add database context
+builder.Services.AddDbContext<ContentActionsDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ContentActionsConnection")));
+
+// Add Elasticsearch
+builder.Services.AddElasticsearch(builder.Configuration);
+
+// Register services
+builder.Services.AddScoped<IUserInteractionService, UserInteractionService>();
+builder.Services.AddScoped<IPersonalizationService, PersonalizationService>();
+builder.Services.AddScoped<IContentService, ContentService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
