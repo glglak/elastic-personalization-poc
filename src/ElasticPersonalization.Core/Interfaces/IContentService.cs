2be6f82399b428afdde7c9aea97c544449dd0faf
glglak <karem.kamel@gmail.com>
2025-03-17 21:17:00 +0200

    Add content service interface

---
+++ src/ElasticPersonalization.Core/Interfaces/IContentService.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ElasticPersonalization.Core.Entities;
+using ElasticPersonalization.Core.Models;
+
+namespace ElasticPersonalization.Core.Interfaces
+{
+    public interface IContentService
+    {
+        // CRUD operations
+        Task<ContentDto> GetContentAsync(string contentId);
+        Task<ContentDto> CreateContentAsync(CreateContentDto contentDto);
+        Task<ContentDto> UpdateContentAsync(string contentId, UpdateContentDto contentDto);
+        Task DeleteContentAsync(string contentId);
+        
+        // Search operations
+        Task<List<ContentDto>> SearchContentAsync(string query, int page = 1, int pageSize = 20);
+        Task<List<ContentDto>> GetContentByCategoryAsync(string category, int page = 1, int pageSize = 20);
+        Task<List<ContentDto>> GetContentByTagAsync(string tag, int page = 1, int pageSize = 20);
+        Task<List<ContentDto>> GetContentByCreatorAsync(int creatorId, int page = 1, int pageSize = 20);
+        
+        // Synchronization with Elasticsearch
+        Task SyncContentToElasticsearchAsync(Content content);
+        Task RemoveContentFromElasticsearchAsync(string contentId);
+    }
+}
