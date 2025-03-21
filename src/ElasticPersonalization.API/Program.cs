3b8d973cbfd769ff7ab96f8e16d1bb684fce2c07
glglak <karem.kamel@gmail.com>
2025-03-21 03:17:42 +0200

    Update Program.cs to initialize database on startup

--- src/ElasticPersonalization.API/Program.cs
+++ src/ElasticPersonalization.API/Program.cs
@@ -1,3 +1,4 @@
+using ElasticPersonalization.API.Data;
 using ElasticPersonalization.API.Extensions;
 using ElasticPersonalization.Core.Interfaces;
 using ElasticPersonalization.Infrastructure.Data;
@@ -5,6 +6,10 @@ using ElasticPersonalization.Infrastructure.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Configure logging
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
 
 // Add services to the container.
 builder.Services.AddControllers();
@@ -26,6 +31,16 @@ builder.Services.AddScoped<IPersonalizationService, PersonalizationService>();
 builder.Services.AddScoped<IContentService, ContentService>();
 
 var app = builder.Build();
+
+// Initialize the database on startup
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    
+    logger.LogInformation("Initializing database...");
+    DbInitializer.Initialize(services, logger);
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
